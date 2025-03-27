88ccb49b0120b81b0f73a519e44196cd8ce15281
kevinh-e <account@kevinh.dev>
2025-03-27 14:50:45 +1100

    updated projects

--- src/app/projects/projects.tsx
+++ src/app/projects/projects.tsx
@@ -1,4 +1,5 @@
 export const projects = [
+  { name: "tastebuds", description: "restaurant recommendation app for indecisive groups of friends", github: "https://github.com/kevinh-e/tastebuds", link: "https://taste-buds-matcher.onrender.com/", tags: ["Next.js", "Framer", "socket.io", "Google Places API"], date: "Mar 26, 2025" },
   { name: "kevinh.dev", description: "this website", github: "https://github.com/kevinh-e/kevinh.dev", link: "", tags: ["Next.js", "Framer"], date: "Dec 6, 2024" },
   { name: "mini-games", description: "collection of web minigames including slider puzzle, minesweeper, tower of hanoi, wordle and snake", github: "https://github.com/kevinh-e/mini-games", link: "https://mini-games.kevinh.dev/", tags: ["Astro", "Framer"], date: "Dec 12, 2024" },
   { name: "presto", description: "deployed minimal slideshow editor project on vercel using a simple redis token database", github: "https://github.com/kevinh-e/presto-deploy", link: "https://z5478535-presto-deploy-wkdw.vercel.app/", tags: ["ReactJS", "Vite"], date: "Nov 15, 2024" },
